28160081e21e153e0f7ce5ba1710038a14cdabb4
JohnyWander <j-myslinski@wp.pl>
2023-05-11 20:33:34 +0200

    launch

--- Program.cs
+++ Program.cs
@@ -1,10 +1,123 @@
-﻿namespace Rock_Paper_Scissors_Multiplayer
+﻿using System.Net;
+using System.Text;
+
+namespace Rock_Paper_Scissors_Multiplayer
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            menu();
+
+
+
+
+
         }
+
+
+        static void menu()
+        {
+
+            Console.WriteLine("1. Connect to server");
+            Console.WriteLine("2. Host new game");
+            Console.WriteLine("3. Options");
+            Console.WriteLine("3. Quit");
+
+            int sw = -1;
+
+            while (sw == -1)
+            {
+                try
+                {//
+                    ConsoleKeyInfo k = Console.ReadKey(true);
+                    sw = int.Parse(k.KeyChar.ToString());
+                }
+                catch (System.FormatException)
+                {
+                    continue;
+                }
+
+            }
+
+
+            switch (sw)
+            {
+
+                case 1:
+                    IPEndPoint endp = GetAdressForConnection();
+
+                    break;
+
+                case 2:
+
+                    break;
+
+
+
+
+
+            }
+
+        }
+
+        static IPEndPoint GetAdressForConnection()
+        {
+            Console.WriteLine("Please enter server ip and port like that - \"123.123.123.235:8000\"\n");
+
+            (int x, int y) = Console.GetCursorPosition();
+            IPEndPoint? add = null;
+
+            while (!IPEndPoint.TryParse(GetUserInput(), out add))
+            {
+                Console.WriteLine("That's not valid address");
+            }
+
+
+
+            return add;
+        }
+
+
+
+
+
+
+
+
+
+        static string GetUserInput()
+        {
+            string input = "";
+
+            input = Console.ReadLine();
+
+
+            return input;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
