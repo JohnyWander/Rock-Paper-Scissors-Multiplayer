1dd473ef8d4fd2f579f744b5056d3e7332639db0
JohnyWander <j-myslinski@wp.pl>
2023-05-11 20:32:45 +0200

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Rock_Paper_Scissors_Multiplayer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
